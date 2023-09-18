54aa0622097776104e78048bfcbaf753b6f553eb
Rishabh Singh <pingrishabh@gmail.com>
2023-09-18 20:28:09 +0530

    Refactored eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
-  root: true,
+  env: {
+    node: true,
+  },
   extends: [
     "universe/native",
     "plugin:react-hooks/recommended",
     "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
+  root: true,
   rules: {
     "import/order": "off",
   },
