0d8929382ba682379a6a3eae279d671e3d921226
Rishabh Singh <pingrishabh@gmail.com>
2023-10-06 18:04:17 +0530

    Restricted pages to the last 24 hours

--- hooks/firestore/usePages.js
+++ hooks/firestore/usePages.js
@@ -1,10 +1,10 @@
 import { useCallback, useMemo } from "react";
 
 import {
+  Timestamp,
   addDoc,
   collection,
   getDocs,
-  limit,
   or,
   orderBy,
   query,
@@ -26,18 +26,28 @@ export default function usePages() {
 
   const getAll = useCallback(
     async (uid) => {
+      const yesterday = Timestamp.fromDate(
+        new Date(Date.now() - 1000 * 60 * 60 * 24),
+      );
+
       const q = query(
-        pages,
-        or(where("from", "==", uid), where("to", "==", uid)),
+        query(pages, or(where("from", "==", uid), where("to", "==", uid))),
+        where("sentAt", ">=", yesterday),
         orderBy("sentAt", "desc"),
-        limit(10),
       );
 
       const results = await getDocs(q);
-      return results.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+      return results.docs.map((doc) => {
+        const page = {
+          id: doc.id,
+          ...doc.data(),
+        };
+
+        page["sentAt"] = page?.sentAt?.toMillis();
+        page["validTill"] = page?.validTill?.toMillis();
+
+        return page;
+      });
     },
     [pages],
   );
