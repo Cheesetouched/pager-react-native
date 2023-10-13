a1795fd448bb6c3d8e43188303153b71cf83cba3
Rishabh Singh <pingrishabh@gmail.com>
2023-10-14 01:00:26 +0530

    Updated page response optimistically

--- hooks/mutations/usePageResponse.js
+++ hooks/mutations/usePageResponse.js
@@ -4,16 +4,32 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import useFirebase from "@hooks/useFirebase";
 import useCoreAction from "@hooks/useCoreAction";
+import useOptimisticUpdate from "@hooks/useOptimisticUpdate";
 
 export default function usePageResponse(props = {}) {
   const { onSuccess } = props;
   const { user } = useFirebase();
   const queryClient = useQueryClient();
+  const update = useOptimisticUpdate();
   const { respondToPage } = useCoreAction();
 
   const { isLoading, mutate } = useMutation((data) => respondToPage(data), {
+    onMutate: ({ pageId, response: { response } }) => {
+      update(["detailedPages", user?.uid], (current) => ({
+        ...current,
+        received: current?.received?.map((page) => {
+          if (page?.id === pageId) {
+            return {
+              ...page,
+              response,
+            };
+          } else {
+            return page;
+          }
+        }),
+      }));
+    },
     onSuccess: () => {
-      //TO-DO: Update this optimisitically
       queryClient.invalidateQueries(["detailedPages", user?.uid]);
 
       if (onSuccess) {
