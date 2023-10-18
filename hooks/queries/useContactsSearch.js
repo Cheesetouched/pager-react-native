2fc3ad075fea56be1f08bd1ed7bb4853d02a2020
Rishabh Singh <pingrishabh@gmail.com>
2023-10-18 15:37:28 +0530

    Removed friends from contact search results

--- hooks/queries/useContactsSearch.js
+++ hooks/queries/useContactsSearch.js
@@ -3,18 +3,35 @@ import { useMemo } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import useAlgolia from "@hooks/useAlgolia";
+import useFirebase from "@hooks/useFirebase";
 
 export default function useContactsSearch(numbers) {
+  const { user } = useFirebase();
   const { contactsSearch } = useAlgolia();
 
   const { data, isInitialLoading, isFetching } = useQuery(
     ["contactsSearch"],
-    async () => contactsSearch(numbers),
+    async () => {
+      const eligibleContacts = [];
+      const search = await contactsSearch(numbers);
+
+      if (search?.results?.length > 0) {
+        search?.results?.map((result) => {
+          if (!result?.friends?.includes(user?.uid)) {
+            eligibleContacts.push(result);
+          }
+        });
+      }
+
+      return { ...search, results: eligibleContacts };
+    },
     {
       enabled:
         numbers !== undefined &&
         typeof numbers === "object" &&
-        numbers?.length > 0,
+        numbers?.length > 0 &&
+        user?.uid !== undefined &&
+        typeof user?.uid === "string",
     },
   );
 
