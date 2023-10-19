f1f8901a5d383051d7f1cc8caa2ebd2ff7af63cb
Rishabh Singh <pingrishabh@gmail.com>
2023-10-19 16:08:44 +0530

    Bumped version and change invite url

--- utils/constants.js
+++ utils/constants.js
@@ -3,6 +3,6 @@ export default {
   ALGOLIA_API_KEY: "0c6d8b85b37e22ff455e34f30157f04f",
   INVITE_LINK: "https://bit.ly/pagr",
   MIXPANEL_PROJECT_TOKEN: "33c509c1fa0bc518072809317dd870c8",
-  PAGER_BASE_URL: "https://pa.ger",
-  PAGER_DOMAIN: "pa.ger",
+  PAGER_BASE_URL: "https://pagr.is",
+  PAGER_DOMAIN: "pagr.is",
 };
