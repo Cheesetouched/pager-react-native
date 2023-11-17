d78004ad36ac88a688c6f456224ae0482ebf7781
Rishabh Singh <pingrishabh@gmail.com>
2023-11-17 12:03:10 +0530

    Updated INVITE_LINK

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 export default {
   ALGOLIA_APP_ID: "D183OQ7HVG",
   ALGOLIA_API_KEY: "0c6d8b85b37e22ff455e34f30157f04f",
-  INVITE_LINK: "https://bit.ly/pagr",
+  INVITE_LINK: "https://apple.co/40HXiTI",
   MIXPANEL_PROJECT_TOKEN: "33c509c1fa0bc518072809317dd870c8",
   PAGER_BASE_URL: "https://pagr.is",
   PAGER_DOMAIN: "pagr.is",
